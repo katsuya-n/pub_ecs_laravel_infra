5afa478b38e242c688e9aec27c068b64b58643b9
中野 克哉 <k_nakano@yumemi.co.jp>
2022-01-08 19:14:50 +0900

    ECS用のsgを作成

--- modules/network/sg/main.tf
+++ modules/network/sg/main.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "alb" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "my ip"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [var.allow_cidr_block]
+    description = "my ip"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.allow_cidr_block]
   }
 
   egress {
@@ -21,5 +21,31 @@ resource "aws_security_group" "alb" {
 
   tags = {
     Name = "${var.name_prefix}-sg-alb"
+  }
+}
+
+resource "aws_security_group" "container" {
+  name        = "${var.name_prefix}-sg-container"
+  description = "sg container"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "alb sg"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-sg-container"
   }
 }
