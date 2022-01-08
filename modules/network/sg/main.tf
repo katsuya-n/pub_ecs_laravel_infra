34b72c5cb5915225915435e3ac2fb4a1b9ef2854
中野 克哉 <k_nakano@yumemi.co.jp>
2022-01-08 19:25:25 +0900

    vpceとrds用のsgを作成

--- modules/network/sg/main.tf
+++ modules/network/sg/main.tf
@@ -49,3 +49,55 @@ resource "aws_security_group" "container" {
     Name = "${var.name_prefix}-sg-container"
   }
 }
+
+resource "aws_security_group" "vpce" {
+  name        = "${var.name_prefix}-sg-vpce"
+  description = "sg vpce"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "container sg"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.container.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-sg-vpce"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "${var.name_prefix}-sg-rds"
+  description = "sg rds"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "container sg"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.container.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-sg-rds"
+  }
+}
