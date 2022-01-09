a00c9214febb7179d88b33111421dfb45c2605ce
中野 克哉 <k_nakano@yumemi.co.jp>
2022-01-09 22:58:16 +0900

    tg追加

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -30,3 +30,22 @@ resource "aws_lb_listener" "blue" {
     target_group_arn = aws_lb_target_group.blue.arn
   }
 }
+
+resource "aws_lb_target_group" "green" {
+  name        = "${var.name_prefix}-green-tg"
+  port        = 10080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_lb_listener" "green" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "10080"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.green.arn
+  }
+}
